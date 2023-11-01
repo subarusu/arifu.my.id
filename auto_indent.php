e67b82c1f0fd32dca96762394a1c49cf055d36b0
subarusu <110146246+subarusu@users.noreply.github.com>
2023-11-02 06:57:36 +0700

    Add files via upload

---
+++ auto_indent.php
@@ -0,0 +1,20 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Ambil teks dari "textbox 1" dan membersihkan masukan
+    $input_text = isset($_POST['input_text']) ? htmlspecialchars($_POST['input_text']) : '';
+
+    // Lakukan auto-indentation, misalnya dengan mengganti baris baru dengan spasi/tab sesuai kebutuhan Anda.
+    $indented_text = autoIndent($input_text);
+
+    // Tampilkan hasil di "textbox 2"
+    echo '<textarea class="textbox" name="output_text" rows="14" cols="60" readonly>' . $indented_text . '</textarea>';
+}
+
+function autoIndent($text) {
+    // Gantilah tanda baris baru dengan spasi/tab sesuai kebutuhan Anda.
+    // Contoh sederhana:
+    $indented_text = str_replace("\n", "  ", $text);
+    
+    return $indented_text;
+}
+?>
